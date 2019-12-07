5ba2e2d0747c5cc32ea40c51216797d1331a2af5
Rysma Aditya W <rysma@loket.com>
2019-12-08 03:06:28 +0700

    Remove articles content from contentful

--- src/@narative/gatsby-theme-novela/templates/articles.template.tsx
+++ src/@narative/gatsby-theme-novela/templates/articles.template.tsx
@@ -1,32 +1,22 @@
 import React from "react";
-import styled from "@emotion/styled";
 
 import Section from "@narative/gatsby-theme-novela/src/components/Section/Section";
 import SEO from "@narative/gatsby-theme-novela/src/components/SEO/SEO";
 import Layout from "@narative/gatsby-theme-novela/src/components/Layout/Layout";
-import Paginator from "@narative/gatsby-theme-novela/src/components/Navigation/Navigation.Paginator";
-import ArticlesHero from "@narative/gatsby-theme-novela/src/sections/articles/Articles.Hero";
-import ArticlesList from "@narative/gatsby-theme-novela/src/sections/articles/Articles.List";
 import Heading from "./../sections/heading/Heading";
 import SiapaKami from "./../sections/siapakami/Siapa.Kami";
 import Podcast from "../components/Podcast/Podcast";
+import Blog from "../sections/articles/Blog";
 
 function ArticlesPage({ location, pageContext }) {
-  const articles = pageContext.group;
-  const authors = pageContext.additionalContext.authors;
-
   return (
     <Layout>
       <SEO title="DeepTech" pathname={location.pathname} />
       <Heading />
       <SiapaKami />
-      <ArticlesHero authors={authors} />
 
       <Section narrow>
-        <ArticlesList articles={articles} />
-        <ArticlesPaginator show={pageContext.pageCount > 1}>
-          <Paginator {...pageContext} />
-        </ArticlesPaginator>
+        <Blog></Blog>
       </Section>
 
       <Podcast />
@@ -35,7 +25,3 @@ function ArticlesPage({ location, pageContext }) {
 }
 
 export default ArticlesPage;
-
-const ArticlesPaginator = styled.div<{ show: boolean }>`
-  ${p => p.show && `margin-top: 95px;`}
-`;
