6e30002a77faabe706ef8b64abd29914e4afc3f2
Rysma Aditya W <rysma@loket.com>
2019-12-08 10:24:01 +0700

    Revert activity rticles

--- src/@narative/gatsby-theme-novela/templates/articles.template.tsx
+++ src/@narative/gatsby-theme-novela/templates/articles.template.tsx
@@ -7,8 +7,15 @@ import Heading from "./../sections/heading/Heading";
 import SiapaKami from "./../sections/siapakami/Siapa.Kami";
 import Podcast from "../components/Podcast/Podcast";
 import Blog from "../sections/articles/Blog";
+import Paginator from "@narative/gatsby-theme-novela/src/components/Navigation/Navigation.Paginator";
+import ArticlesList from "@narative/gatsby-theme-novela/src/sections/articles/Articles.List";
+import ArticlesHero from "@narative/gatsby-theme-novela/src/sections/articles/Articles.Hero";
+import styled from "@emotion/styled";
 
 function ArticlesPage({ location, pageContext }) {
+  const articles = pageContext.group;
+  const authors = pageContext.additionalContext.authors;
+
   return (
     <Layout>
       <SEO title="DeepTech" pathname={location.pathname} />
@@ -19,9 +26,22 @@ function ArticlesPage({ location, pageContext }) {
         <Blog></Blog>
       </Section>
 
+      <ArticlesHero authors={authors} />
+
+      <Section narrow>
+        <ArticlesList articles={articles} />
+        <ArticlesPaginator show={pageContext.pageCount > 1}>
+          <Paginator {...pageContext} />
+        </ArticlesPaginator>
+      </Section>
+
       <Podcast />
     </Layout>
   );
 }
+
+const ArticlesPaginator = styled.div<{ show: boolean }>`
+  ${p => p.show && `margin-top: 95px;`}
+`;
 
 export default ArticlesPage;
